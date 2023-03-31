79023b813dcae8149f44e203a7c5bd4fbccf7907
sankarihari <129526439+sankarihari@users.noreply.github.com>
2023-03-31 20:17:22 +0530

    Initial commit

---
+++ Basics1.js
@@ -0,0 +1,87 @@
+// a=5;
+// console.log(a);
+// console.error('Danger');
+// var a=['sankari','hari',26];
+// console.log(a.length);
+// console.log(a[2]);
+//  let person={
+    // age:'26',
+    // qualification:'graduation',
+    // name: "sankari",
+// }
+// console.log(person.age);
+// console.log(person.name);
+// var man = new Object();
+// man.name="hari";
+// man.age = 30;
+// console.log(man);
+ 
+// var months = ["jan","feb","march","april"];
+// console.log(months);
+    
+// var Users = ["sankari","hari",26];
+// console.log(Users);
+
+// function welcome() {
+    // console.log("Hello world");
+// }
+// welcome();
+
+//  let a = 5;
+//  let b = 10;
+//  let c = a+b;
+// console.log("sum is" + c);
+
+// function sum(x,y){
+    // let z = x+y ;
+    // console.log("sum is" +z);
+// }
+// sum(2,4);
+// sum(5,10);
+
+// let a = 2;
+// let b = 4;
+
+// Arithmatic operators
+// Addition operator
+// let add = a + b;
+// console.log(add);
+// Substraction operator
+// let sub = a-b;
+// console.log(sub);
+// Multiplication operator
+// let multi = a*b;
+// console.log(multi);
+// Division operator
+// let div = a/b;
+// console.log(div);
+// Modular operator / Reminder operator
+// let modu = a%b;
+// console.log(modu);
+// incremet
+// a++;
+// console.log(a);
+// decrement
+// b--
+// console.log(b);
+
+// postfix
+
+// let s=2;
+// let h=s++;
+ 
+// console.log(h);
+
+// prefix
+// let c=++h;
+// console.log(c);
+
+// Assignment operator
+// let a=2;
+// console.log(a+=3);     addition
+
+// console.log(a-=1);   substraction
+
+// console.log(a*=2);    multipli
+
+//    console.log(a/=4);    
---
+++ script.js
@@ -0,0 +1,35 @@
+// console.log("HEllo world");
+
+// let i=20;
+// console.log(i);
+
+// for(let i=0; i<=10; i++){
+//     console.log(i);
+// }
+// for(let i=10; i>=0; i--){
+//     console.log(i);
+// }
+
+
+// let sum=0;
+// for (let i=1; i<=10;i++){
+//     sum=sum+i
+// }
+// console.log("sum of 1st natural number is" ,sum);
+
+// let i = 0;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+let i =2;
+do{
+    console.log(i);
+}while(i>2);
+
+// for of loop
+
+let students=["Hari","Sankari","Parvathy"]
+for(let student of students){
+    console.log(`Hello ${student}`)
+}
